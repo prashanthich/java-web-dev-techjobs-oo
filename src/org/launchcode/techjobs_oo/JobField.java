6dde6fc515dd434821106cafe051813d84403759
Prashanthi Chirunomula <chirunomulap@gmail.com>
2020-12-20 14:15:27 -0600

    Code refactered

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,43 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public JobField(String aValue) {
+        this();
+        this.value = aValue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
+}
--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,25 +1,8 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
-
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
-    public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return this.value;
+public class PositionType extends JobField{
+    public PositionType(String value){
+        super(value);
     }
 
     @Override
@@ -27,27 +10,8 @@ public class PositionType {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id;
+        return getId() == that.getId();
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-    //  when
-    //  their id fields match.
 
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
